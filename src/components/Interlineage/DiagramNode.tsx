76c0d98edf256d4d68418e6465d3ca55d2542970
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-05-22 06:05:15 +0200

    feature - InterlineageDetail and DiagramNode customized

--- src/components/Interlineage/DiagramNode.tsx
+++ src/components/Interlineage/DiagramNode.tsx
@@ -1,15 +1,34 @@
 // @ts-ignore
 import { Handle } from 'react-flow-renderer';
 import './DiagramNode.scss'
+
 import { memo } from 'react';
 
 // @ts-ignore
-const DiagramNode = memo(({ data, isConnectable }) => {
+const DiagramNode = memo((props) => {
+  // @ts-ignore
+  const { data, id, isConnectable } = props;
+  const { body = null, showHideNodes, title = '...', positionHandleSource = 'bottom', positionHandleTarget = 'left' } = data;
+
   return (
     <div className="diagramNode">
-      <div className="diagramNode-header">Title</div>
-      <div className="diagramNode-body">Body</div>
-      custom node
+      <Handle
+        type="target"
+        // @ts-ignore
+        position={positionHandleTarget}
+        style={{ background: '#555' }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={isConnectable}
+      />
+      {title && (<div className="diagramNode-header">{title}</div>)}
+      {body && <div className="diagramNode-body">{body}</div>}
+      <Handle
+        type="source"
+        id={id}
+        isConnectable={isConnectable}
+        // @ts-ignore
+        position={positionHandleSource}
+      />
     </div>
   );
 });
--- src/screens/InterlineageDetail.tsx
+++ src/screens/InterlineageDetail.tsx
@@ -1,174 +1,184 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge, Controls, MarkerType } from 'react-flow-renderer';
+import ReactFlow, { Background, useNodesState, useEdgesState, addEdge, Controls, MarkerType } from 'react-flow-renderer';
 import DiagramNode from 'components/Interlineage/DiagramNode';
-
-const initBgColor = '#dedede';
 
 const connectionLineStyle = { stroke: '#fff' };
 const snapGrid = [20, 20];
 
+const NODE_TYPES = {
+  customNode: DiagramNode,
+};
+
+const CUSTOM_STYLE = {
+  border: '2px solid rgb(45, 204, 205)',
+}
+
+const EDGES = [
+  {
+    id: '1-2',
+    source: '1',
+    target: '2',
+    markerEnd: {
+      type: MarkerType.Arrow,
+    },
+  },
+  {
+    id: '1a-1b',
+    source: '1a',
+    target: '1b',
+    markerEnd: {
+      type: MarkerType.Arrow,
+    },
+  },
+  {
+    id: '2-3',
+    source: '2',
+    target: '3',
+    markerEnd: {
+      type: MarkerType.Arrow,
+    },
+  }
+];
+
 const InterlineageDetail = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [bgColor, setBgColor] = useState(initBgColor);
 
-  const nodeTypes = {
-    customNode: DiagramNode,
+
+  const showHideNodes = (id: string) => {
+    const nodeIsVisible = nodes.findIndex((node) => node.id === id);
+    if (nodeIsVisible) {
+      const nextNodes = nodes.filter((node) => node.id !== id);
+      setNodes(nextNodes);
+    } else {
+      const nextNode = NODES.find((node) => node.id === id);
+      // @ts-ignore
+      const nextNodes = nodes.concat([nextNode]);
+      setNodes(nextNodes);
+    }
+  }
+
+  const genericData = {
+    showHideNodes
   };
 
+  const NODES = [
+    {
+      type: 'customNode',
+      id: '1',
+      data: { ...genericData, positionHandleSource: 'right', title: 'F   CONTRATOS' },
+      // @ts-ignore
+      sourcePosition: 'right',
+      style: {
+        ...CUSTOM_STYLE,
+        backgroundColor: 'rgba(255, 255, 255, 0.35)',
+        height: 350,
+        width: 480
+      },
+      position: { x: 100, y: 200 },
+    },
+    {
+      id: '1a',
+      data: { ...genericData, title: 'P INTERVINIENTES DOMÉSTICA', positionHandleSource: 'right' },
+      // @ts-ignore
+      sourcePosition: 'right',
+      parentNode: '1',
+      position: {
+        x: 15, y: 65
+      },
+      style: {
+        ...CUSTOM_STYLE,
+        width: 300,
+        height: 200,
+      },
+      type: 'customNode',
+    },
+    {
+      id: '1b',
+      data: { ...genericData, body: 'EOM CONTRATOS' },
+      // @ts-ignore
+      targetPosition: 'left',
+      parentNode: '1',
+      position: {
+        x: 340, y: 145
+      },
+      style: {
+        ...CUSTOM_STYLE,
+      },
+      type: 'customNode',
+    },
+    {
+      id: '1a1',
+      data: { ...genericData, body: 'EOM INTERVINIENTES' },
+      extent: 'parent',
+      parentNode: '1a',
+      position: {
+        x: 15, y: 50
+      },
+      style: {
+        ...CUSTOM_STYLE,
+      },
+      type: 'customNode',
+    },
+    {
+      id: '1a2',
+      data: { ...genericData, body: 'EOM INTERVINIENTES ES DOMÉSTICA' },
+      extent: 'parent',
+      parentNode: '1a',
+      position: {
+        x: 15, y: 120
+      },
+      style: {
+        ...CUSTOM_STYLE,
+      },
+      type: 'customNode',
+    },
+    {
+      id: '2',
+      data: { ...genericData, title: 'P   CONTRATOS' },
+      // @ts-ignore
+      targetPosition: 'left',
+      // @ts-ignore
+      sourcePosition: 'bottom',
+      style: {
+        ...CUSTOM_STYLE,
+        backgroundColor: 'rgba(255, 255, 255, 0.35)',
+        width: 200,
+        height: 200,
+      },
+      position: { x: 650, y: 250 },
+      type: 'customNode',
+    },
+    {
+      id: '2a',
+      data: { ...genericData, title: 'CONTRATOS CLIENTE' },
+      extent: 'parent',
+      parentNode: '2',
+      position: {
+        x: 25, y: 70
+      },
+      style: {
+        ...CUSTOM_STYLE,
+      },
+      type: 'customNode',
+    },
+    {
+      id: '3',
+      data: { ...genericData, body: 'EOM CONTRATOS ACTIVOS', positionHandleTarget: 'top' },
+      style: {
+        ...CUSTOM_STYLE
+      },
+      position: { x: 675, y: 500 },
+      type: 'customNode',
+    }
+  ];
+
+
   useEffect(() => {
-    const onChange = (event: any) => {
-      setNodes((nds) =>
-        nds.map((node) => {
-          if (node.id !== '2') {
-            return node;
-          }
+    // @ts-ignore
+    setNodes(NODES);
 
-          const color = event.target.value;
-
-          setBgColor(color);
-
-          return {
-            ...node,
-            data: {
-              ...node.data,
-              color,
-            },
-          };
-        })
-      );
-    };
-
-    setNodes([
-      {
-        id: '1',
-        data: { label: 'F   CONTRATOS' },
-        // @ts-ignore
-        sourcePosition: 'right',
-        style: {
-          backgroundColor: 'rgba(255, 255, 255, 0.35)',
-          border: '1px solid #777',
-          height: 350,
-          padding: 10,
-          width: 450
-        },
-        position: { x: 100, y: 200 },
-      },
-      {
-        id: '1a',
-        data: { label: 'P INTERVINIENTES DOMÉSTICA' },
-        // @ts-ignore
-        sourcePosition: 'right',
-        parentNode: '1',
-        position: {
-          x: 15, y: 65
-        },
-        style: {
-          width: 200,
-          height: 200,
-        }
-      },
-      {
-        id: '1b',
-        data: { label: 'EOM CONTRATOS' },
-        // @ts-ignore
-        targetPosition: 'left',
-        parentNode: '1',
-        position: {
-          x: 250, y: 145
-        }
-      },
-      {
-        id: '1a1',
-        data: { label: 'EOM INTERVINIENTES' },
-        extent: 'parent',
-        parentNode: '1a',
-        position: {
-          x: 15, y: 50
-        }
-      },
-      {
-        id: '1a2',
-        data: { label: 'EOM INTERVINIENTES ES DOMÉSTICA' },
-        extent: 'parent',
-        parentNode: '1a',
-        position: {
-          x: 15, y: 100
-        },
-      },
-      {
-        id: '2',
-        data: { label: 'P   CONTRATOS' },
-        // @ts-ignore
-        targetPosition: 'left',
-        // @ts-ignore
-        sourcePosition: 'bottom',
-        style: {
-          backgroundColor: 'rgba(255, 255, 255, 0.35)',
-          border: '1px solid #777',
-          padding: 10,
-          width: 200,
-          height: 200,
-        },
-        position: { x: 650, y: 250 },
-      },
-      {
-        id: '2a',
-        data: { label: 'CONTRATOS CLIENTE' },
-        extent: 'parent',
-        parentNode: '2',
-        position: {
-          x: 25, y: 70
-        },
-      },
-      {
-        id: '3',
-        data: { label: 'EOM CONTRATOS ACTIVOS' },
-        style: {
-          backgroundColor: 'rgba(255, 255, 255, 0.35)',
-          border: '1px solid #777',
-          padding: 10
-        },
-        position: { x: 675, y: 500 },
-      },
-      {
-        id: '4',
-        data: { label: 'CUSTOM NODE' },
-        style: {
-          backgroundColor: 'white',
-          border: '2px solid rgb(45, 204, 205)',
-        },
-        type: 'customNode',
-        position: { x: 100, y: 700 },
-      },
-    ]);
-
-    setEdges([
-      {
-        id: '1-2',
-        source: '1',
-        target: '2',
-        markerEnd: {
-          type: MarkerType.Arrow,
-        },
-      },
-      {
-        id: '2-3',
-        source: '2',
-        target: '3',
-        markerEnd: {
-          type: MarkerType.Arrow,
-        },
-      },
-      {
-        id: '1a-1b',
-        source: '1a',
-        target: '1b',
-        markerEnd: {
-          type: MarkerType.Arrow,
-        },
-      }
-    ]);
+    setEdges(EDGES);
   }, []);
 
   const onConnect = useCallback(
@@ -180,11 +190,11 @@ const InterlineageDetail = () => {
     <ReactFlow
       nodes={nodes}
       edges={edges}
-      /*nodeTypes={nodeTypes}*/
+      nodeTypes={NODE_TYPES}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
-      style={{ background: bgColor }}
+      style={{ background: 'rgb(255 255 255)' }}
       connectionLineStyle={connectionLineStyle}
       snapToGrid={true}
       // @ts-ignore
@@ -192,6 +202,8 @@ const InterlineageDetail = () => {
       defaultZoom={1.5}
       attributionPosition="bottom-left"
     >
+      {/* @ts-ignore */}
+      <Background />
       <Controls />
     </ReactFlow>
   );
